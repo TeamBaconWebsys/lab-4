9a6c70cdf39307ac0fd2837d3cd8711abe52d730
callumhauber <callumhauber@gmail.com>
2021-10-08 04:06:26 -0400

    fixed navbar stuff

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,33 @@ var nav_links = document.querySelector(".navbar-nav");
 
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 50) {
-    nav.classList.add('bg-dark', 'shadow', 'navbar-dark');
-    nav_links.classList.add('link-light');
-    nav_links.classList.remove('link-dark');
+    nav.classList.add('bg-dark', 'shadow');
   } else {
-    nav.classList.remove('bg-dark', 'shadow', 'navbar-dark');
-    nav_links.classList.add('link-dark');
-    nav_links.classList.remove('link-light');
+    nav.classList.remove('bg-dark', 'shadow');
   }
 });
 
 $(document).ready(function () {
 
   //getting ajax through getJSON function
-  $.getJSON("text.json", function(data) {
-    $.each(data.Tutoring, function(i, element) {
-      $("#tutoring").append("<h2>"+element.h1+"</h2>");
-      $("#tutoring").append("<p>"+element.p1+"</p>");
-      $("#tutoring").append("<p>"+element.p2+"</p>");
-      $("#tutoring").append("<p>"+element.p3+"</p>");
+  $.getJSON("text.json", function (data) {
+    $.each(data.Tutoring, function (i, element) {
+      $("#tutoring").append("<h2>" + element.h1 + "</h2>");
+      $("#tutoring").append("<p>" + element.p1 + "</p>");
+      $("#tutoring").append("<p>" + element.p2 + "</p>");
+      $("#tutoring").append("<p>" + element.p3 + "</p>");
     });
 
-    $.each(data.Expose, function(i, element) {
-      $("#expose").append("<h2>"+element.h1+"</h2>");
-      $("#expose").append("<p>"+element.p1+"</p>");
-      $("#expose").append("<p>"+element.p2+"</p>");
-      $("#expose").append("<p>"+element.p3+"</p>");
+    $.each(data.Expose, function (i, element) {
+      $("#expose").append("<h2>" + element.h1 + "</h2>");
+      $("#expose").append("<p>" + element.p1 + "</p>");
+      $("#expose").append("<p>" + element.p2 + "</p>");
+      $("#expose").append("<p>" + element.p3 + "</p>");
     });
 
-    $.each(data.Newsletter, function(i, element) {
-      $("#newsletter").append("<h2>"+element.h1+"</h2>");
-      $("#newsletter").append("<p>"+element.p1+"</p>");
+    $.each(data.Newsletter, function (i, element) {
+      $("#newsletter").append("<h2>" + element.h1 + "</h2>");
+      $("#newsletter").append("<p>" + element.p1 + "</p>");
     });
   });
 });
