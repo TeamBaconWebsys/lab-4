99281b1701cc500b21e23458337c87d54140da1a
callumhauber <callumhauber@gmail.com>
2021-10-08 00:44:59 -0400

    working navbar + animations

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,29 @@
-$(document).ready(function() {
-   
-    //getting ajax through getJSON function
+var nav = document.querySelector('nav');
+var nav_links = document.querySelector(".navbar-nav");
 
-    $.getJSON("text.json", function(result){
-        $(result).find("socialMedia").each(function(){
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 50) {
+    nav.classList.add('bg-dark', 'shadow', 'navbar-dark');
+    nav_links.classList.add('link-light');
+    nav_links.classList.remove('link-dark');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow', 'navbar-dark');
+    nav_links.classList.add('link-dark');
+    nav_links.classList.remove('link-light');
+  }
+});
 
-            $("#footer").html(
-                '<svg xmlns="' + element.xmlns + '" class="' + element.class + '">' + 
-                '<path d="' + element.path + '"/> </svg>'
-            )
-        });
+$(document).ready(function () {
+
+  //getting ajax through getJSON function
+
+  $.getJSON("text.json", function (result) {
+    $(result).find("socialMedia").each(function () {
+
+      $("#footer").html(
+        '<svg xmlns="' + element.xmlns + '" class="' + element.class + '">' +
+        '<path d="' + element.path + '"/> </svg>'
+      )
     });
+  });
 });
